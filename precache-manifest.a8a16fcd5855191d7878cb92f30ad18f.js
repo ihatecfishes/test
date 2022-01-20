f20214d027b1147609befcfd00ffb597bb3ef028
cheesehao <anha.tra@gmail.com>
2022-01-20 21:42:24 +0700

    Updates

---
+++ precache-manifest.a8a16fcd5855191d7878cb92f30ad18f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878b8bb98d4a3dc64adedc86418b402a",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2c815bef4044e70a7d54",
+    "url": "/test/static/css/2.fbf08e0d.chunk.css"
+  },
+  {
+    "revision": "d2c685a8ba120fa167ec",
+    "url": "/test/static/css/main.84edd9cd.chunk.css"
+  },
+  {
+    "revision": "2c815bef4044e70a7d54",
+    "url": "/test/static/js/2.183753ff.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/test/static/js/2.183753ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c685a8ba120fa167ec",
+    "url": "/test/static/js/main.d1de09dd.chunk.js"
+  },
+  {
+    "revision": "ff9c426cd2ca9da05db7",
+    "url": "/test/static/js/runtime-main.ca6075c2.js"
+  },
+  {
+    "revision": "c874fabbf6af6023c11bc105ec5d6ffd",
+    "url": "/test/static/media/me.c874fabb.jpg"
+  }
+]);
